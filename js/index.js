693d6db0eb9a5e4540188b4c34fd38f50385e383
jisankhan5 <jk1483777@gmail.com>
2025-08-27 12:52:52 +0600

    add functionality coin section

--- js/index.js
+++ js/index.js
@@ -9,54 +9,25 @@ for (const heartBtn of heartBtns) {
   });
 }
 
-// call button section
-const subTitle1 = document
-  .getElementById("call-btn1")
-  .addEventListener("click", function () {
-    alert('Calling National Emergency 999....')
-  });
-const subTitle2 = document
-  .getElementById("call-btn2")
-  .addEventListener("click", function () {
-    alert('Calling Police 999....')
-  });
-const subTitle3 = document
-  .getElementById("call-btn3")
-  .addEventListener("click", function () {
-    alert('Calling Fire Service 999....')
-  });
-const subTitle4 = document
-  .getElementById("call-btn4")
-  .addEventListener("click", function () {
-    alert('Calling Ambulance 1994-999999....')
-  });
-const subTitle5 = document
-  .getElementById("call-btn5")
-  .addEventListener("click", function () {
-    alert('Calling Woman & Child Helpline 109....')
-  });
-const subTitle6 = document
-  .getElementById("call-btn6")
-  .addEventListener("click", function () {
-    alert('Calling Anti-Corruption 106....')
-  });
-const subTitle7 = document
-  .getElementById("call-btn7")
-  .addEventListener("click", function () {
-    alert('Calling Electricity Outage 16216....')
-  });
-const subTitle8 = document
-  .getElementById("call-btn8")
-  .addEventListener("click", function () {
-    alert('Calling Brac 16445....')
-  });
-const subTitle9 = document
-  .getElementById("call-btn9")
-  .addEventListener("click", function () {
-    alert('Calling Bangladesh Railway 163....')
-  });
-
+//   call button  section
 const callBtns = document.getElementsByClassName("call-btn");
 for (const callBtn of callBtns) {
-  callBtn.addEventListener("click", function () {});
+  callBtn.addEventListener("click", function () {
+    const balance = parseInt(document.getElementById("coin-btn").innerText);
+    const availabelBalance = balance - Number(20);
+    console.log(availabelBalance);
+    
+    
+    if(balance < 20){
+        alert(`❌You don't have enough coins!It takes at least 20 coins to make a call.`)
+    }
+    else{
+        const parant = callBtn.parentNode.parentNode.children[1].children[0].children[1].innerText
+        const Number =callBtn.parentNode.parentNode.children[1].children[1].children[0].innerText
+         alert(`📞Calling ${parant} ${Number}....`)
+        document.getElementById("coin-btn").innerText = availabelBalance;
+    }
+  });
+   
 }
+
